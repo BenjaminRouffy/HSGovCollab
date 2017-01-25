b35a7a3ad1d783830145e3c6cd55051ea962897a
Anton Rylov <anton.rylov@propeople.com.ua>
2017-01-25 16:43:56 +0200

    Create jquery plugin 'visible' and add animation for blockquotes

--- docroot/themes/custom/ample/js/main.js
+++ docroot/themes/custom/ample/js/main.js
@@ -1,4 +1,31 @@
 (function($, Drupal) {
   'use strict';
 
+  Drupal.behaviors.fadeOut = {
+    attach: function(context, settings) {
+      var $blockquote = $('blockquote');
+
+      $blockquote.each(function(i, el) {
+        var $el = $(el);
+
+        if (!$el.isVisible(true)) {
+          $el.css({
+            'opacity': 0,
+            'transform': 'translateY(15px)'
+          });
+        }
+      });
+
+      $(window).scroll(function() {
+        $blockquote.each(function(i, el) {
+          var $el = $(el);
+
+          if ($el.isVisible(true)) {
+            $el.addClass("visible");
+          }
+        });
+      });
+    }
+  };
+
 })(jQuery, Drupal);

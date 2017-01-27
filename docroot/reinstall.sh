ce4dc8c285115781db86b18b5024dfd9dfcc1e7f
Yuriy Boychenko <yuboychenko@gmail.com>
2017-01-27 13:04:21 +0200

    syntax fix

--- docroot/reinstall.sh
+++ docroot/reinstall.sh
@@ -4,3 +4,4 @@ if [ "$1" = "--windows" ]; then
     time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local --extra-vars "is_windows=true"
 else
     time ansible-playbook -vvvv reinstall.yml -i 'localhost,' --connection=local
+fi

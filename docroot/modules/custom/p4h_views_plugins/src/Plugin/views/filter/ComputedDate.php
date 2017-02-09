7b0a4f9c1e6f82e92ade3ee3b1ca9588afc18037
Yuriy Boychenko <yuboychenko@gmail.com>
2017-02-09 13:54:38 +0200

    Computed & CustomCombine filters

--- docroot/modules/custom/p4h_views_plugins/src/Plugin/views/filter/ComputedDate.php
+++ docroot/modules/custom/p4h_views_plugins/src/Plugin/views/filter/ComputedDate.php
@@ -2,35 +2,17 @@
 
 namespace Drupal\p4h_views_plugins\Plugin\views\filter;
 
-use DateTime;
 use Drupal\Core\Datetime\DateHelper;
+use DateTime;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\views\Plugin\views\filter\NumericFilter as NumericFilterDefault;
+use Drupal\views\Plugin\views\filter\ManyToOne;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Date/time views filter.
- *
- * Even thought dates are stored as strings, the numeric filter is extended
- * because it provides more sensible operators.
- *
  * @ingroup views_filter_handlers
- *
- * @ViewsFilter("datetime")
+ * @ViewsFilter("computed_date")
  */
-class DateSelect extends NumericFilterDefault implements ContainerFactoryPluginInterface {
-
-  /**
-   * @var \Drupal\views\Plugin\views\query\Sql
-   */
-  public $query;
-
-  /**
-   * Mark form as extra setting form.
-   */
-  public function hasExtraOptions() {
-    return TRUE;
-  }
+class ComputedDate extends ManyToOne {
 
   /**
    * Default settings.
@@ -44,6 +26,12 @@ class DateSelect extends NumericFilterDefault implements ContainerFactoryPluginI
     $options['from_unixtime'] = array('default' => TRUE);
 
     return $options;
+  }
+  /**
+   * Mark form as extra setting form.
+   */
+  public function hasExtraOptions() {
+    return TRUE;
   }
 
   /**
@@ -79,48 +67,44 @@ class DateSelect extends NumericFilterDefault implements ContainerFactoryPluginI
     );
 
   }
-
-  /**
-   * Add a type selector to the value form.
-   */
-  protected function valueForm(&$form, FormStateInterface $form_state) {
-    parent::valueForm($form, $form_state);
-
-    if ($exposed = $form_state->get('exposed') && $this->options['type'] == 'select') {
-      $form['value']['#type'] = 'select';
-      $form['value']['#options'] = $this->selectOptions();
-      $form['value']['#empty_option'] = $this->granularityOptions()[$this->options['granularity']];
-      $form['value']['#size'] = 1;
-
-    }
-  }
-
-  /**
-   * Make some translations to a form item to make it more suitable to exposing.
-   */
-  protected function exposedTranslate(&$form, $type) {
-    parent::exposedTranslate($form, $type);
-
-    if ($type == 'value' && empty($this->always_required) && empty($this->options['expose']['required']) && $form['#type'] == 'select' && empty($form['#multiple'])) {
-      unset($form['#options']['All']);
-    }
-
-  }
-
-  /**
-   * Helper operation filter.
-   */
-  public function operators() {
+  function operators() {
     $operators = array(
-      '=' => array(
-        'title' => $this->t('Is equal to'),
+      'in' => array(
+        'title' => $this->t('Is one of'),
+        'short' => $this->t('in'),
+        'short_single' => $this->t('='),
         'method' => 'opSimple',
-        'short' => $this->t('='),
         'values' => 1,
       ),
     );
 
     return $operators;
+  }
+
+  protected function opSimple() {
+    if (empty($this->value)) {
+      return;
+    }
+    $this->ensureMyTable();
+
+    $field = "$this->tableAlias.$this->realField";
+
+    $granularity = $this->options['granularity'];
+    $placeholder = $this->placeholder();
+
+    $this->query->addWhereExpression($this->options['group'], "$granularity($field) {$this->operator} ({$placeholder}[])", [
+      "{$placeholder}[]" => array_values($this->value)
+    ]);
+  }
+
+  /**
+   * Helper granularity option.
+   */
+  private function granularityOptions() {
+    return array(
+      'year' => t('Year', array(), array('context' => 'datetime')),
+      'month' => t('Month', array(), array('context' => 'datetime')),
+    );
   }
 
   /**
@@ -139,7 +123,7 @@ class DateSelect extends NumericFilterDefault implements ContainerFactoryPluginI
         $end->modify("$end_year year");
         $year = range($start->format('Y'), $end->format('Y'));
         $options = array_combine($year, $year);
-        asort($options);
+        arsort($options);
         break;
 
       case 'month':
@@ -149,27 +133,9 @@ class DateSelect extends NumericFilterDefault implements ContainerFactoryPluginI
     return $options;
   }
 
-  /**
-   * Singe operator query alter.
-   */
-  protected function opSimple($field) {
-    if ($this->value['value']) {
-      $key = ':value' . rand(100, 999);
-      $granularity = $this->options['granularity'];
-      $from_unixtime = $this->options['from_unixtime'] ? 'from_unixtime' : '';
-
-      $this->query->addWhereExpression($this->options['group'], "$granularity($from_unixtime($field)) = $key", [$key => $this->value['value']]);
-    }
-  }
-
-  /**
-   * Helper granularity option.
-   */
-  private function granularityOptions() {
-    return array(
-      'year' => t('Year', array(), array('context' => 'datetime')),
-      'month' => t('Month', array(), array('context' => 'datetime')),
-    );
+  public function getValueOptions() {
+    $this->valueOptions = $this->selectOptions();
+    return $this->valueOptions;
   }
 
 }

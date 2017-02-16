6f8dd18c228fd685a13810b5df2f74807cbe2f9c
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2017-02-16 15:02:47 +0200

    Custom: Add custom block 'Map'

---
+++ docroot/modules/custom/country/src/Plugin/Block/Map.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\country\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class Map.
+ *
+ * @Block(
+ *   id = "map",
+ *   category = @Translation("Custom"),
+ *   admin_label = @Translation("Map")
+ * )
+ */
+class Map extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'key' => '',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Google API key'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['key'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    foreach ($this->defaultConfiguration() as $key => $default_value) {
+      $this->configuration[$key] = $form_state->getValue($key);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    $build['#markup'] = '<div id="map"></div>';
+    $build['#attached']['drupalSettings']['map'] = $this->configuration;
+
+    return $build;
+  }
+
+}

c0bade1ec21361d1acdf48a884614f0c4c8596ed
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2017-02-20 12:06:10 +0200

    Custom: Add template for document in timeline and set computed date

---
+++ docroot/modules/custom/p4h_views_plugins/src/Plugin/ComputedDate/Document.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\p4h_views_plugins\Plugin\ComputedDate;
+
+use Drupal\p4h_views_plugins\ComputedDateBase;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * @ComputedDate(
+ *   id = "document",
+ *   name = @Translation("Document"),
+ * )
+ */
+class Document extends ComputedDateBase {
+
+  /**
+   * @return \Drupal\Core\Datetime\DrupalDateTime
+   */
+  public function getValue() {
+    /* @var $entity EntityInterface */
+    $entity = $this->getEntity();
+
+    if (!empty($entity->field_content_date) && isset($entity->field_content_date->date)) {
+      /* @var \Drupal\Core\Datetime\DrupalDateTime */
+      return $entity->field_content_date->date;
+    }
+  }
+
+}

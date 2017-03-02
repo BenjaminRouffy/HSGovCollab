d8fc45b795ff2007ebc3310f7c36c57d5114e6b4
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2017-03-02 18:33:58 +0200

    Custom: Add validate by permissions

--- docroot/modules/custom/group_dashboard/src/Plugin/views/filter/NodeAccess.php
+++ docroot/modules/custom/group_dashboard/src/Plugin/views/filter/NodeAccess.php
@@ -60,22 +60,24 @@ class NodeAccess extends FilterPluginBase {
    */
   public function query() {
     $account = \Drupal::currentUser();
-    $op = $this->options['node_operations'];
+    if (!$account->hasPermission('bypass node access')) {
+      $op = $this->options['node_operations'];
 
-    $table = $this->ensureMyTable();
-    $grants = new Condition('OR');
+      $table = $this->ensureMyTable();
+      $grants = new Condition('OR');
 
-    foreach (node_access_grants($op, $account) as $realm => $gids) {
-      foreach ($gids as $gid) {
-        $grants->condition((new Condition('AND'))
-          ->condition($table . '.gid', $gid)
-          ->condition($table . '.realm', $realm)
-        );
+      foreach (node_access_grants($op, $account) as $realm => $gids) {
+        foreach ($gids as $gid) {
+          $grants->condition((new Condition('AND'))
+            ->condition($table . '.gid', $gid)
+            ->condition($table . '.realm', $realm)
+          );
+        }
       }
-    }
 
-    $this->query->addWhere('AND', $grants);
-    $this->query->addWhere('AND', $table . ".grant_$op", 1, '>=');
+      $this->query->addWhere('AND', $grants);
+      $this->query->addWhere('AND', $table . ".grant_$op", 1, '>=');
+    }
   }
 
   /**
@@ -84,7 +86,7 @@ class NodeAccess extends FilterPluginBase {
   public function getCacheContexts() {
     $contexts = parent::getCacheContexts();
 
-    $contexts[] = 'user.node_grants:update';
+    $contexts[] = 'user.node_grants:' . $this->options['node_operations'];
 
     return $contexts;
   }

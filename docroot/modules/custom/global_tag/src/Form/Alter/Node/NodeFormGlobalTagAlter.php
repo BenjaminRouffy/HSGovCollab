3f3f3e3e9fee888e83fcd09be47d4cb64f0089df
Yuriy Boychenko <yuboychenko@gmail.com>
2017-03-03 11:54:09 +0200

    Set default value

--- docroot/modules/custom/global_tag/src/Form/Alter/Node/NodeFormGlobalTagAlter.php
+++ docroot/modules/custom/global_tag/src/Form/Alter/Node/NodeFormGlobalTagAlter.php
@@ -1,15 +1,17 @@
 <?php
 namespace Drupal\global_tag\Form\Alter\Node;
 
+use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Field\FieldFilteredMarkup;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\form_alter_service\FormBaseAlterHelper;
 use Drupal\form_alter_service\Interfaces\FormAlterServiceAlterInterface;
+use Drupal\group\Entity\Group;
+use Drupal\node\Entity\Node;
 
 class NodeFormGlobalTagAlter extends FormBaseAlterHelper implements FormAlterServiceAlterInterface {
 
   public function formAlter(&$form, FormStateInterface $form_state) {
-    }
 
     // Show the fieldset if we have options the user can use.
     $form['custom_publish_options'] = array(
@@ -23,7 +25,17 @@ class NodeFormGlobalTagAlter extends FormBaseAlterHelper implements FormAlterSer
       '#optional' => TRUE,
     );
 
-    $form['field_public_content']['widget']['#default_value'] = '_none';
+    $form_object = $form_state->getFormObject();
+    if ($form_object instanceof EntityForm) {
+      $parent_entity = $form_object->getEntity();
+    }
+    if (empty($parent_entity) || ($parent_entity instanceof Node && $parent_entity->id() == NULL)) {
+      $storage = $form_state->getStorage();
+      if (isset($storage['group']) && $storage['group'] instanceof Group) {
+        $parent_group = $storage['group']->id();
+      }
+      $form['field_public_content']['widget']['#default_value'] = !isset($parent_group) ? 'global' : '_none';
+    }
 
     $form['field_public_content']['widget']['#options']['_none'] = FieldFilteredMarkup::create('Country');
     $form['field_public_content']['#group'] = 'custom_publish_options';

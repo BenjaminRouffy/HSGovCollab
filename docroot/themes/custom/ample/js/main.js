2fbae97ccc80790c5a600abd37b26a6c248f7c35
Anton Rylov <anton.rylov@propeople.com.ua>
2017-03-03 13:49:33 +0200

    Fix js for ie

--- docroot/themes/custom/ample/js/main.js
+++ docroot/themes/custom/ample/js/main.js
@@ -125,26 +125,25 @@
     }
   };
 
+  function showRelated() {
+    var $window = $(window);
+    var $document = $(document);
+    var scrollTop = $window.scrollTop();
+    var halfDocOffset = $document.height() / 2;
+    var halfWinOffset = $window.height() / 2;
+
+    if ((scrollTop + halfWinOffset) > halfDocOffset) {
+      $relatedContent.addClass('visible');
+    } else {
+      $relatedContent.removeClass('visible');
+    }
+  }
+
   Drupal.behaviors.relatedContent = {
     attach: function(context, settings) {
       var $relatedContent = $('.related-wrapper', context);
 
       if ($relatedContent.length) {
-
-        function showRelated() {
-          var $window = $(window);
-          var $document = $(document);
-          var scrollTop = $window.scrollTop();
-          var halfDocOffset = $document.height() / 2;
-          var halfWinOffset = $window.height() / 2;
-
-          if ((scrollTop + halfWinOffset) > halfDocOffset) {
-            $relatedContent.addClass('visible');
-          } else {
-            $relatedContent.removeClass('visible');
-          }
-        }
-
         showRelated();
 
         $(window).scroll(function() {

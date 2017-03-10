fea87d3a2033293e4cb5c508e4afc8da60819501
Yuriy Borisov <yuriy.borisov@propeople.com.ua>
2017-03-10 12:40:41 +0200

    new group filter

--- docroot/modules/custom/group_dashboard/src/Plugin/views/filter/ParentGroupSelect.php
+++ docroot/modules/custom/group_dashboard/src/Plugin/views/filter/ParentGroupSelect.php
@@ -16,14 +16,34 @@ class ParentGroupSelect extends GroupIndexByGroupType  {
   /**
    * {@inheritdoc}
    */
-  protected function valueForm(&$form, FormStateInterface $form_state) {
-    $group_type = $this->groupType->load($this->options['gid']);
+  public function buildExtraOptionsForm(&$form, FormStateInterface $form_state) {
+    $group_type = $this->groupType->loadMultiple();
     $options = [];
-    $result = [];
 
-    $user = \Drupal::currentUser();
+    foreach ($group_type as $entity_type) {
+      $options[$entity_type->id()] = $entity_type->label();
+    }
 
+    $form['gid'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Group type'),
+      '#options' => $options,
+      '#default_value' => $this->options['gid'],
+      '#required' => TRUE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function valueForm(&$form, FormStateInterface $form_state) {
+    $group_types = $this->groupType->loadMultiple($this->options['gid']);
+    $group_types_id = [];
+    foreach ($group_types as $type) {
+      $group_types_id[] = $type->id();
+    }
     $default_value = (array) $this->value;
+    $options = [];
 
     if ($exposed = $form_state->get('exposed')) {
       $identifier = $this->options['expose']['identifier'];
@@ -63,40 +83,10 @@ class ParentGroupSelect extends GroupIndexByGroupType  {
       // @todo Sorting on vocabulary properties -
       //   https://www.drupal.org/node/1821274.
       ->addTag('group_access');
-    $query->condition('type', $group_type->id());
-
-    if (!$user->hasPermission('all access to groups')) {
-      $parent_group = $group_type->id();
-      $parent_query = \Drupal::database()->select('group_content_field_data', 'group_content_field_data')
-        ->fields('group_content_field_data', ['gid'])
-        ->condition('group_content_field_data.type', "$parent_group-group_membership", 'LIKE')
-        ->condition('group_content_field_data.entity_id', $user->id());
-
-      $parent_query->leftJoin('group_content__group_roles', 'group_content__group_roles', 'group_content__group_roles.entity_id=group_content_field_data.id');
-      $result = $parent_query->condition('group_content__group_roles.group_roles_target_id', '%-admin', 'LIKE')
-        ->execute()
-        ->fetchCol();
-
-      // Not show any group is user not has admin access.
-      $result = empty($result) ? [0] : $result;
-    }
-
+    $query->condition('type', $group_types_id, 'IN');
     $groups = $this->group->loadMultiple($query->execute());
 
-    foreach ($groups as $group) {
-      if (!$user->hasPermission('all access to groups')) {
-        if (array_search($group->id(), $result) !== FALSE) {
-          $options[$group->id()] = \Drupal::entityManager()
-            ->getTranslationFromContext($group)
-            ->label();
-        }
-      }
-      else {
-        $options[$group->id()] = \Drupal::entityManager()
-          ->getTranslationFromContext($group)
-          ->label();
-      }
-    }
+    $options = $this->getUserGroupMembership($group_types, $groups);
 
     asort($options);
     $form_state->set('filter_options', $options);
@@ -118,5 +108,60 @@ class ParentGroupSelect extends GroupIndexByGroupType  {
       $form_state->setUserInput($user_input);
     }
 
+  }
+
+  /**
+   * Check user's membership in Group.
+   *
+   * @param array $group_types
+   *   Array of group types.
+   * @param array $groups
+   *   Array of groups.
+   *
+   * @return array
+   *   Return array of user's groups.
+   */
+  protected function getUserGroupMembership($group_types, $groups) {
+    $result = [];
+    $options = [];
+
+    $user = \Drupal::currentUser();
+
+    if (!$user->hasPermission('all access to groups')) {
+      foreach ($group_types as $group_type) {
+        $parent_group = $group_type->id();
+        $parent_query = \Drupal::database()
+          ->select('group_content_field_data', 'group_content_field_data')
+          ->fields('group_content_field_data', ['gid'])
+          ->condition('group_content_field_data.type', "$parent_group-group_membership", 'LIKE')
+          ->condition('group_content_field_data.entity_id', $user->id());
+
+        $parent_query->leftJoin('group_content__group_roles', 'group_content__group_roles', 'group_content__group_roles.entity_id=group_content_field_data.id');
+        $query_result = $parent_query->condition('group_content__group_roles.group_roles_target_id', '%-admin', 'LIKE')
+          ->execute()
+          ->fetchCol();
+
+        if (!empty($query_result)) {
+          $result = array_merge($result, $query_result);
+        }
+      }
+    }
+
+    foreach ($groups as $group) {
+      if (!$user->hasPermission('all access to groups')) {
+        if (array_search($group->id(), $result) !== FALSE) {
+          $options[$group->id()] = \Drupal::entityManager()
+            ->getTranslationFromContext($group)
+            ->label();
+        }
+      }
+      else {
+        $options[$group->id()] = \Drupal::entityManager()
+          ->getTranslationFromContext($group)
+          ->label();
+      }
+    }
+
+    return $options;
   }
 }

9c83ea2fe73914917571e30ddb5d274441e4e31a
Yuriy Boychenko <yuboychenko@gmail.com>
2017-03-15 11:05:03 +0200

    Form Alter Service

---
+++ docroot/modules/sandbox/form_alter_service/src/Interfaces/FormAlterServiceAlterInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\form_alter_service\Interfaces;
+
+use Drupal\Core\Form\FormStateInterface;
+
+interface FormAlterServiceAlterInterface {
+
+  /**
+   * Form alter custom implementation.
+   * @param $form
+   * @param FormStateInterface $form_state
+   */
+  public function formAlter(&$form, FormStateInterface $form_state);
+
+}

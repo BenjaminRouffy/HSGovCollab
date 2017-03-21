31c4b29bc012cf995bad8fe6bc5570d3c4687be5
Anton Rylov <anton.rylov@propeople.com.ua>
2017-03-21 11:12:35 +0200

    P4H-417

--- docroot/themes/custom/ample/js/main.js
+++ docroot/themes/custom/ample/js/main.js
@@ -177,6 +177,7 @@
           $anchorLink.each(function(index, element) {
             var title = $(element).find('.anchor-title').first().text();
             var id = title.trim();
+            var $el = $(element);
 
             // Remove  special characters from string.
             id = id.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '_');
@@ -184,6 +185,39 @@
             element.id = id;
 
             $bottomHead.find('.anchor-links ul').append('<li><a href="#' + id + '">' + title + '</a></li>');
+
+            $(window).on('scroll', function() {
+              var lastId;
+              var menuItems = $('.anchor-links').find("a");
+
+              // Anchors corresponding to menu items
+              var scrollItems = menuItems.map(function(){
+                var item = $($(this).attr("href"));
+                if (item.length) { return item; }
+              });
+
+              // Get container scroll position
+              var fromTop = $(this).scrollTop();
+
+              // Get id of current scroll item
+              var cur = scrollItems.map(function(){
+                if ($(this).offset().top < fromTop)
+                  return this;
+              });
+
+              // Get the id of the current element
+              cur = cur[cur.length-1];
+              var id = cur && cur.length ? cur[0].id : "";
+
+              if (lastId !== id) {
+                lastId = id;
+
+                // Set/remove active class
+                menuItems
+                  .parent().removeClass("active")
+                  .end().filter("[href='#" + id + "']").parent().addClass("active");
+              }
+            });
           });
         }
 

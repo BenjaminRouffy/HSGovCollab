6c80ceec948d97b23dad6db1ad2ab1bc385d2dbf
Valeriy Garzha <valerasebafr1@gmail.com>
2017-03-28 13:16:18 +0300

    Custom formatter for group_content

---
+++ docroot/modules/custom/group_content_formatters/src/Plugin/Field/FieldFormatter/GroupContentCountry.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\group_content_formatters\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\group\Entity\Group;
+
+/**
+ * Plugin implementation of the 'plugin_reference_id' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "group_content_country_list",
+ *   label = @Translation("Group content country."),
+ *   field_types = {
+ *     "group_content_item"
+ *   }
+ * )
+ */
+class GroupContentCountry extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode):array {
+    $elements = [];
+
+    // Initialize field.
+    $items->setValue(1);
+    $global_content = $items->getEntity()->global_content->getValue();
+
+    if (isset($global_content[0]) && $global_content[0]['value'] == TRUE) {
+      $elements[0][] = [
+        '#type' => 'markup',
+        '#markup' => $this->t('Global'),
+      ];
+    }
+    else {
+      foreach ($items as $delta => $item) {
+        $value = $item->getValue();
+
+        if (count($value['entity_gids']) > 1) {
+          $elements[0][] = [
+            '#type' => 'markup',
+            '#markup' => $this->t('Multiple countries'),
+          ];
+        }
+        else {
+          foreach (Group::loadMultiple($value['entity_gids']) as $group) {
+            $elements[$delta][] = [
+              '#type' => 'link',
+              '#title' => $group->label(),
+              '#url' => Url::fromRoute('entity.group.canonical', ['group' => $group->id()]),
+            ];
+          }
+        }
+      }
+
+    }
+
+
+    return $elements;
+  }
+
+}

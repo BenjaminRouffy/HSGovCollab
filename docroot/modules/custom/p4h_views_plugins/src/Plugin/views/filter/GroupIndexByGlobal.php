1dcbfee59130785a9fe13c5ba896d268c69b4006
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2017-04-11 18:15:32 +0300

    Custom: Create custom view plugin for views 'My countrs/regions/projects'

---
+++ docroot/modules/custom/p4h_views_plugins/src/Plugin/views/filter/GroupIndexByGlobal.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\p4h_views_plugins\Plugin\views\filter;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Config\Entity\ConfigEntityStorageInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\group\Entity\Group;
+use Drupal\group\Entity\GroupContent;
+use Drupal\group\Entity\GroupTypeInterface;
+use Drupal\group\Entity\Storage\GroupContentStorageInterface;
+use Drupal\group_following\GroupFollowingManagerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\views\Annotation\ViewsFilter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Filter by term id.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("group_index_by_global")
+ */
+class GroupIndexByGlobal extends GroupIndexGid  {
+
+  /**
+   * @var GroupTypeInterface
+   */
+  public $groupType;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition,ConfigEntityStorageInterface $group_type) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->groupType = $group_type;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.manager')->getStorage('group_type')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function hasExtraOptions() {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['gid'] = ['default' => ''];
+
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildExtraOptionsForm(&$form, FormStateInterface $form_state) {
+    $group_type = $this->groupType->loadMultiple();
+    $options = [];
+
+    foreach ($group_type as $entity_type) {
+      $options[$entity_type->id()] = $entity_type->label();
+    }
+
+    $form['gid'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('Group type'),
+      '#options' => $options,
+      '#default_value' => $this->options['gid'],
+      '#required' => TRUE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function valueForm(&$form, FormStateInterface $form_state) {
+    $options = [];
+    $account = \Drupal::currentUser();
+    $groupPermissionAccess = \Drupal::getContainer()
+      ->get('group_customization.group.permission');
+
+    $query = \Drupal::entityQuery('node')
+      // @todo Sorting on vocabulary properties -
+      //   https://www.drupal.org/node/1821274.
+      ->addTag('node_access')
+      ->condition('type', 'news')
+      ->condition('global_content', TRUE);
+    $result = $query->execute();
+
+    $result = \Drupal::database()->select('group_content_field_data', 'group_content_field_data')
+      ->fields('group_content_field_data', ['gid'])
+      ->condition('entity_id', $result, 'IN')
+      ->execute()
+      ->fetchCol();
+
+    /* @var GroupContent $group_content */
+    foreach ($result as $group_id) {
+      $group = Group::load($group_id);
+
+      if (!in_array($group->bundle(), $this->options['gid'])) {
+        continue;
+      }
+
+      $access = $groupPermissionAccess->checkAccessForFilter($group, $account, ['published', 'content',]);
+
+      if ($access->isAllowed()) {
+        $options[$group->id()] = \Drupal::entityManager()
+          ->getTranslationFromContext($group)
+          ->label();
+      }
+    }
+
+    asort($options);
+    $form_state->set('filter_options', $options);
+
+    parent::valueForm($form, $form_state);
+  }
+
+}

d583e696ce6311116c397ad803cdb4c40d873323
Yuriy Borisov <yuriy.borisov@propeople.com.ua>
2017-04-18 12:13:36 +0300

    fix test comments field display mode

--- docroot/modules/custom/ajax_comments/tests/src/FunctionalJavascript/AjaxCommentsFunctionalTest.php
+++ docroot/modules/custom/ajax_comments/tests/src/FunctionalJavascript/AjaxCommentsFunctionalTest.php
@@ -46,7 +46,7 @@ class AjaxCommentsFunctionalTest extends JavascriptTestBase {
     $this->addDefaultCommentField('node', 'article');
 
     // Enable ajax comments on the comment field.
-    $entity_view_display = EntityViewDisplay::load('node.article.default');
+    $entity_view_display = EntityViewDisplay::load('node.article.comments');
     $renderer = $entity_view_display->getRenderer('comment');
     $renderer->setThirdPartySetting('ajax_comments', 'enable_ajax_comments', TRUE);
     $entity_view_display->save();

6811d37444f636535b381bfb9e676c0c40f3d769
Vladmir Gaydamaka <vladimir.gaydamaka@gmail.com>
2017-04-18 19:15:25 +0300

    Custom: Hide following button on group type

--- docroot/modules/custom/group_dashboard/src/Form/Alter/GroupTypeAlter.php
+++ docroot/modules/custom/group_dashboard/src/Form/Alter/GroupTypeAlter.php
@@ -37,6 +37,12 @@ class GroupTypeAlter implements FormAlterServiceBaseInterface, FormAlterServiceA
       '#default_value' => $group_type->getThirdPartySetting('group_dashboard', 'access_to_subgroup_functionality', 0),
     ];
 
+    $form['hide_follow_button'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Hide follow button on group'),
+      '#default_value' => $group_type->getThirdPartySetting('group_dashboard', 'hide_follow_button', 0),
+    ];
+
     // Process the submitted values before they are stored.
     $form['#entity_builders'][] = [$this, 'setFollowingSettings'];
   }
@@ -46,6 +52,7 @@ class GroupTypeAlter implements FormAlterServiceBaseInterface, FormAlterServiceA
    */
   function setFollowingSettings($entity_type, GroupType $group, &$form, FormStateInterface $form_state) {
     $group->setThirdPartySetting('group_dashboard', 'access_to_subgroup_functionality', $form_state->getValue('access_to_subgroup_functionality'));
+    $group->setThirdPartySetting('group_dashboard', 'hide_follow_button', $form_state->getValue('hide_follow_button'));
   }
 
 }

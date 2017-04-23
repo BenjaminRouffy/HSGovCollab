db146a9f988439ac8be10706343dc5bb4edd6b5f
Yuriy Boychenko <yuboychenko@gmail.com>
2017-04-23 12:21:45 +0300

    Patch: getting all followers by group id

--- docroot/modules/custom/group_following/src/GroupFollowingStorage.php
+++ docroot/modules/custom/group_following/src/GroupFollowingStorage.php
@@ -38,22 +38,25 @@ class GroupFollowingStorage implements GroupFollowingStorageInterface {
    *   GroupFollowing::buildJoin.
    */
   public function buildJoin(JoinPluginBase $join_plugin, $select_query, $table, $view_query) {
-    $select = $this->buildJoinQuery(\Drupal::currentUser()->getAccount());
-    $select_query->join($select, 'group_select', db_and()
-      ->where("{$join_plugin->leftTable}.{$join_plugin->leftField} = group_select.gid")
+    $select = $this->buildJoinQuery();
+
+    $select_query->join($select, $table['alias'], db_and()
+      ->where("{$join_plugin->leftTable}.{$join_plugin->leftField} = {$table['alias']}.{$join_plugin->field}")
+//      ->condition("group_select.uid", \Drupal::currentUser()->getAccount()->id())
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildJoinQuery(AccountInterface $account) {
+  public function buildJoinQuery() {
     $select_query = db_select($this->sqlBuilder->groupUnion(), 'group_union');
     $select_query->addField('group_union', 'end_vertex', 'gid');
+    $select_query->addField('group_following', 'uid', 'uid');
     /** @var \Drupal\Core\Database\Query\Select $select_query */
     $alias = 'group_union';
 
-    $select_query->leftJoin($this->sqlBuilder->groupFollowing($account), 'group_following', db_and()
+    $select_query->leftJoin($this->sqlBuilder->groupFollowing(), 'group_following', db_and()
       ->where("{$alias}.start_vertex = group_following.gid"));
 
     $select_query->leftJoin('group_graph', 'group_graph_1', db_and()
@@ -61,10 +64,12 @@ class GroupFollowingStorage implements GroupFollowingStorageInterface {
       ->where("{$alias}.exit_edge_id = group_graph_1.id"));
 
 
-    $select_query->leftJoin($this->sqlBuilder->groupFollowing($account), 'group_following_start', db_and()
-      ->where("group_graph_1.start_vertex = group_following_start.gid"));
-    $select_query->leftJoin($this->sqlBuilder->groupFollowing($account), 'group_following_end', db_and()
-      ->where("group_graph_1.end_vertex = group_following_end.gid"));
+    $select_query->leftJoin($this->sqlBuilder->groupFollowing(), 'group_following_start', db_and()
+      ->where("group_graph_1.start_vertex = group_following_start.gid")
+      ->where("group_following.uid = group_following_start.uid"));
+    $select_query->leftJoin($this->sqlBuilder->groupFollowing(), 'group_following_end', db_and()
+      ->where("group_graph_1.end_vertex = group_following_end.gid")
+      ->where("group_following_start.uid = group_following_end.uid"));
 
     $select_query->addExpression('CONCAT_WS(\',\', group_union.start_vertex,
             if(group_union.start_vertex != group_graph_1.start_vertex, group_graph_1.start_vertex, NULL),
@@ -93,8 +98,11 @@ class GroupFollowingStorage implements GroupFollowingStorageInterface {
    *   Count of following references.
    */
   public function getFollowerByGroupForUser(GroupInterface $group, AccountInterface $account) {
-    $select = $this->buildJoinQuery($account);
-    $condition = db_and()->condition("group_union.end_vertex", $group->id());
+    $db_name = $this->buildJoinQuery();
+    $select = db_select($db_name, 'group_select');
+    $select->fields('group_select');
+    $condition = db_and()->condition("group_select.gid", $group->id())
+      ->condition("group_select.uid", $account->id());
     $select->condition($condition);
     $result = $select->countQuery();
     return $result->execute()->fetchField();
@@ -104,7 +112,10 @@ class GroupFollowingStorage implements GroupFollowingStorageInterface {
    * {@inheritdoc}
    */
   public function getFollowedForUser(AccountInterface $account) {
-    $select = $this->buildJoinQuery($account);
+    $db_name = $this->buildJoinQuery();
+    $select = db_select($db_name, 'group_select');
+    $select->fields('group_select');
+    $select->condition("group_select.uid", $account->id());
     $result = $select->execute()->fetchCol();
     return $result;
   }

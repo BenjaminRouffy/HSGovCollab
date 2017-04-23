e4e0c9b51a80cd562701ee7d9eb38e18ac4fa75b
Yuriy Boychenko <yuboychenko@gmail.com>
2017-04-23 12:21:45 +0300

    Patch: getting all followers by group id

--- docroot/modules/custom/group_following/src/Helper/Sql/Builder.php
+++ docroot/modules/custom/group_following/src/Helper/Sql/Builder.php
@@ -79,7 +79,8 @@ class Builder {
   /**
    * {@inheritdoc}
    */
-  public function groupFollowing(AccountInterface $account) {
+  public function groupFollowing() {
+
     $select = db_select('group_content_field_data', 'gcf');
     $select->addField('gcf', 'id', 'id');
     $select->addField('gcf', 'gid', 'gid');
@@ -92,7 +93,7 @@ class Builder {
       ->where('gcf.id = gcff.entity_id'));
 
     // @TODO Replace to $account.
-    $select->condition('gcf.entity_id', $account->id());
+    // $select->condition('gcf.entity_id', $account->id());
     return $select;
   }
 

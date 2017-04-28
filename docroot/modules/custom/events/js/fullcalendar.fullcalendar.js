dd92bb3dc33684114646cbedb32d9bd50cef8a81
Yuriy Boychenko <yuboychenko@gmail.com>
2017-04-29 00:37:58 +0300

    Calendar views

--- docroot/modules/custom/events/js/fullcalendar.fullcalendar.js
+++ docroot/modules/custom/events/js/fullcalendar.fullcalendar.js
@@ -1,11 +1,7 @@
-(function ($, Drupal, drupalSettings) {
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
+(function ($, Drupal) {
   Drupal.behaviors.eventsCalendar = {
     attach: function (context, settings) {
-      var options = jQuery.extend(true, {
+      var options = {
         header: {
           left: 'month year',
           center: 'prev title next',
@@ -16,38 +12,23 @@
         firstDay: 1,
         eventLimit: true,
         weekNumbers: true,
-        columnFormat: 'dddd'
-      }, {
-        eventClick: function (event, jsEvent, view) {
-          //set the values and open the modal
-          // @TODO
-          // $(".event-content").html(event.description);
-          // $(".event-content").dialog({modal: true, title: event.title});
-        },
+        columnFormat: 'dddd',
         events: function (start, end, timezone, callback) {
-          // @TODO Add ajax request here to reduce the site of data.
-          // ex. /user/my-calendar/json
           var events = [];
-          // @TODO Change element ID to class, it is not allow use few calenar on same page.
-          $('#' + settings.calendar.wrapper + ' .views-row').each(function () {
+          $('.view-my-calendar .views-row').each(function () {
+            var times = $(this).find('.event-date time');
             events.push({
-              // @TODO Delete completely.
-              title: $(this).find('.views-field-title .field-content').text().trim(),
-              start: $(this).find('.views-field-field-date .field-content time:eq(0)').attr('datetime').trim(),
-              end: $(this).find('.views-field-field-date .field-content time:eq(1)').attr('datetime').trim(),
-              color: 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + ')',
-              description: $(this).find('.views-field-rendered-entity .field-content').html()
+              title: $(this).find('.event-title').text().trim(),
+              start: $(times).eq(0).attr('datetime').trim(),
+              end: $(times).eq(1).attr('datetime').trim(),
             });
             $(this).hide();
           });
+          this.options.useajax = true;
           callback(events);
         }
-      });
-
-      // @TODO Add a ability to attach multiple calendars.
-      $('#' + settings.calendar.wrapper).fullCalendar(options);
-
+      };
+      $('#calendar').fullCalendar(options);
     }
   }
-
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal);

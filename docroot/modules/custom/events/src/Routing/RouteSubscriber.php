bbf3a53f9577ab6fcb9a861a336c769dddc652d9
Yuriy Boychenko <yuboychenko@gmail.com>
2017-05-15 02:09:55 +0300

    GA sublink access check

--- docroot/modules/custom/events/src/Routing/RouteSubscriber.php
+++ docroot/modules/custom/events/src/Routing/RouteSubscriber.php
@@ -20,7 +20,7 @@ class RouteSubscriber extends RouteSubscriberBase {
     foreach (['group.calendar', 'view.my_calendar.data_export_1'] as $route_name) {
       if ($route = $collection->get($route_name)) {
         $route->addRequirements([
-          // @TODO Create customaccess service (https://www.drupal.org/docs/8/api/routing-system/access-checking-on-routes)
+          // @TODO Create custom access service (https://www.drupal.org/docs/8/api/routing-system/access-checking-on-routes)
           '_custom_access' => "\\Drupal\\events\\CalendarAccessControlHandler::checkMembershipAccess",
         ]);
       }

2c5f43d89a0516f673624e16923f76628a6ebad9
Vitaly Malashevsky <vitaly.malashevsky@ffwagency.com>
2017-11-07 18:11:47 +0200

    P4H-833: Implement custom date_range_slider to display date fields as slider

--- docroot/modules/contrib/facets/src/Plugin/facets/facet_source/SearchApiBaseFacetSource.php
+++ docroot/modules/contrib/facets/src/Plugin/facets/facet_source/SearchApiBaseFacetSource.php
@@ -154,6 +154,7 @@ abstract class SearchApiBaseFacetSource extends FacetSourcePluginBase {
       case 'date':
         $query_types['date'] = 'search_api_date';
         $query_types['range'] = 'search_api_range';
+        $query_types['date_range'] = 'search_api_date_range';
         break;
 
       case 'decimal':
